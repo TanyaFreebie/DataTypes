2ebb035d86402966e70288a0a1530c1183269137
TanyaFreebie <fjodrova.tanya@gmail.com>
2021-06-18 20:40:58 +0300

    First Lesson Idea

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	   // this are the veriables I decleared
+	    int firstNumber;
+	    int secondNumber;
+	    //this is where I initialize them
+	    firstNumber = 5;
+	    secondNumber =6;
+
+	    int sum = firstNumber + secondNumber;
+//	    System.out.println (sum);
+
+
+	    float firstFloat = 0.5f;
+	    float secondFloat =2.3f;
+	    float floatSum = firstFloat + secondFloat;
+//	    System.out.println(floatSum);
+
+	    double firstDouble = 0.7;
+		double secondDouble = 2.4;
+	    double doubleSum = firstDouble + secondDouble;
+//	    System.out.println (doubleSum);
+
+		String firstString = "Chips";
+		String secondString = "Chicken";
+//		System.out.println();
+
+		
+
+
+
+    }
+}
